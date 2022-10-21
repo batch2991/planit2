151a695b82006803ab6ed522d4df68b602af4588
venkatgn <venkatgn.gutta@gmail.com>
2022-10-21 11:00:12 +0530

    1st time commit

---
+++ Sample2/src/main/java/pages/Google.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Google 
+{
+	 WebDriver driver;
+	 
+	  public void search()
+	  {
+		  String sbrowser=System.getProperty("browsername");
+		  
+		  if(sbrowser=="chrome")
+		  {		  
+			  WebDriverManager.chromedriver().setup();
+			  driver=new ChromeDriver();
+		  }
+		   if(sbrowser=="firefox")
+		  {		  
+			  WebDriverManager.firefoxdriver().setup();
+			  driver=new FirefoxDriver();
+		  }		  
+		  driver.manage().window().maximize();
+		  driver.get("http://www.google.com");
+		  driver.findElement(By.name("q")).sendKeys("java");
+		  driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+	  }
+  }
+
